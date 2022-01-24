93dc1f6e8984c28e65a457a58b0b5c5fb89eb9db
KingPrime69 <quentin.ripot@free.fr>
2022-01-24 16:03:41 +0100

    exo 3 c'est la mort

--- qripot/exo/Exercice3/exercice3.hpp
+++ qripot/exo/Exercice3/exercice3.hpp
@@ -9,28 +9,11 @@ class MainSDLWindow{
     public:
     MainSDLWindow();
     ~MainSDLWindow();
-    int Init(const char *title, int x, int y);
+    int Init(const char *title, int x, int y, int w);
     SDL_Renderer *GetRenderer(void);
 
     private:
     SDL_Window *window;
     SDL_Renderer *renderer;
 };
-
-void keyboard() {
-  const Uint8 *keystates = SDL_GetKeyboardState(NULL);
-
-  if (keystates[SDL_SCANCODE_UP]) {
-     ...
-  }
-  if (keystates[SDL_SCANCODE_DOWN]) {
-    ...
-  }
-  if (keystates[SDL_SCANCODE_LEFT]) {
-    ...
-  }
-  if (keystates[SDL_SCANCODE_RIGHT]) {
-    ...
-  }
-}
 #endif
